58be62c656fadf77ffa9f01271b95121afacb552
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-14 21:14:35 -0500

    ready for deployment

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r dependencies.txt
+
+## Run migrations in cas any migrations hadn't been run yet
+python manage.py migrate
+
