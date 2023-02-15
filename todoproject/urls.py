2b2a84eee3d84bbf411cbd8fc05571f9fcf95d2e
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-14 19:58:46 -0500

    add routes

--- todoproject/urls.py
+++ todoproject/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from todos.views import TodoViewSet
+
+# Create a Router
+router = routers.DefaultRouter()
+# Register viewset with the router
+router.register(r'todos')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
